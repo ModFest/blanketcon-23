cd108b20e0d23db27068f2d1f662b87ecc97a971
Una Thompson <me@unascribed.com>
2023-08-20 02:53:08 -0700

    Further insulation, fix QSL coming from CurseForge

--- bunnify.sh
+++ bunnify.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
 # Bunnify
-# Replaces any GitHub links found in the mods folder with a link to the Bunny bucket instead, for stability.
+# Replaces any unreliable links found in the mods folder with a link to the Bunny bucket instead, for stability.
 # Rehashes the pack afterwards.
 
-echo "Converting any stray GitHub links to the Bunny bucket..."
-cd mods
-for f in *.pw.toml; do sed -i 's/github.com/blanketcon.b-cdn.net/g' $f; done
-cd ..
+echo "Converting any stray links to the Bunny bucket..."
+for repl in \
+	'https://github.com/=https://blanketcon.b-cdn.net/' \
+	'https://mvn.devos.one/=https://blanketcon.b-cdn.net/devos/' \
+	'https://ci.blamejared.com/=https://blanketcon.b-cdn.net/jared/' \
+	'https://jaskarth.com/=https://blanketcon.b-cdn.net/jaskarth/' \
+	'https://maven.ithundxr.dev/=https://blanketcon.b-cdn.net/ithundxr/' \
+	; do
+	find -name '*.pw.toml' -print0 | xargs -0 sed -i "s=$repl=g"
+done
 packwiz refresh
 echo "Conversion done!"
